ad43bbdb004494a159696d088e2b516b147c0f90
fedecogo <141748020+fedecogo@users.noreply.github.com>
2023-11-28 18:28:48 +0100

    ciao

--- src/Main.java
+++ src/Main.java
@@ -13,5 +13,11 @@ public class Main {
             // for you, but you can always add more by pressing Ctrl+F8.
             System.out.println("i = " + i);
         }
+        for (int i = 1; i <= 5; i++) {
+
+            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+            // for you, but you can always add more by pressing Ctrl+F8.
+            System.out.println("i = " + i);
+        }
     }
 }
